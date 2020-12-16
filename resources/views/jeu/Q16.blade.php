d600876ed6355ec3694ac1f375ed9a1872be71c2
henri_delbaere <henri_delbaere@ens.univ-artois.fr>
2020-12-16 15:44:15 +0100

    question16 corrigé, 19 pas totalement

---
+++ resources/views/jeu/Q16.blade.php
@@ -0,0 +1,16 @@
+<title>informations du jeu</title>
+<h2>MONOPOLY</h2>
+<p>{{$j->url_media}}</p>
+<p>{{$j->theme_id}}</p>
+<p>Mécanique : PLUMER TOUT LE MONDE </p>
+<p>{{$j->description}}</p>
+<p>Catégorie : {{$j->categorie}}</p>
+<p>Langue : {{$j->langue}}</p>
+<p>Editeur : {{$j->editeur}}</p>
+<p>Nombre de joueur : {{$j->nombre_joueurs}}</p>
+<p>Durée : {{$j->duree}}</p>
+</div>
+
+
+
+
