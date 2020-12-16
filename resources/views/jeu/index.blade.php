8f12c6e9612aa7df8cd639b795c72f688acfceda
thomasnoeson <54883700+thomasnoeson@users.noreply.github.com>
2020-12-16 22:34:47 +0100

    Affichage liste Q 27/28/29

--- resources/views/jeu/index.blade.php
+++ resources/views/jeu/index.blade.php
@@ -4,16 +4,61 @@
 
 @section('content')
 
-    <h1 class="text-center">Tous les jeux de la super ludotheque de l'IUT de Lens</h1>
+    <h1 class="text-center">Tous les jeux de la super ludotheque de l'IUT de Lens</h1><br>
     <div class="row">
-        <div class="col-6 text-left">
-            @auth
-                <a class="btn btn-success" href="{{ URL::route('jeu_create') }}">Ajouter un jeu</a>
-            @endauth
+        <div>
+            <div>
+                <a style="margin-left: 2%;" class="btn btn-success" href="{{ URL::route('jeu_create') }}">Ajouter un jeu</a>
+            </div>
+            <br>
+            <div>
+                <a style="margin-left: 2%;" href="{{ URL::route('jeu_index', $sort) }}">Trié par nom @if ($filter !== null)<i class="fas  @if ($sort == 0)fa-sort-down @else fa-sort-up @endif "></i> @endif</a>
+            </div>
+            <br>
+            <div style="margin-left: 2%;">
+                <h3>Choix de l'éditeur</h3>
+                <select name="choixEditeur" size="1">
+                    @foreach( \App\Models\Editeur::all() as $editeur)
+                        @if (old('editeur') == $editeur->id)
+                            <option value="{{ $editeur->id }}" selected>{{ $editeur->nom }}</option>
+                        @else
+                            <option value="{{ $editeur->id }}">{{ $editeur->nom }}</option>
+                        @endif
+                    @endforeach
+                </select><br />
+                <input type="button" onclick='window.location.reload(false)' value="Rechercher"/>
+            </div>
+            <br>
+            <div style="margin-left: 2%;">
+                <h3>Choix du thème</h3>
+                <select name="choixTheme" size="1">
+                    @foreach( \App\Models\Theme::all() as $theme)
+                        @if (old('theme') == $theme->id)
+                            <option value="{{ $theme->id }}" selected>{{ $theme->nom }}</option>
+                        @else
+                            <option value="{{ $theme->id }}">{{ $theme->nom }}</option>
+                        @endif
+                    @endforeach
+                </select><br />
+                <input type="button" onclick='window.location.reload(false)' value="Rechercher"/>
+            </div>
+            <br>
+            <div style="margin-left: 2%;">
+                <h3>Choix du mécanisme</h3>
+                <select name="choixMecanisme" size="1">
+                    @foreach( \App\Models\Mecanique::all() as $mecanique)
+                        @if (old('mecanique') == $mecanique->id)
+                            <option value="{{ $mecanique->id }}" selected>{{ $mecanique->nom }}</option>
+                        @else
+                            <option value="{{ $mecanique->id }}">{{ $mecanique->nom }}</option>
+                        @endif
+                    @endforeach
+                </select><br>
+                <input type="button" onclick='window.location.reload(false)' value="Rechercher"/>
+            </div>
+            <br>
         </div>
-        <div class="col-6 text-right">
-            <a href="{{ URL::route('jeu_index', $sort) }}">Trié par nom @if ($filter !== null)<i class="fas  @if ($sort == 0)fa-sort-down @else fa-sort-up @endif "></i> @endif</a>
-        </div>
+
     </div>
     <div class="row ">
 
