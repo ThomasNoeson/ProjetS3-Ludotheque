6afbe0ffa5d8ba917cdcafa381475c2fedd7178c
Belval Melchior <melchior_belval@ens.univ-artois.fr>
2020-12-16 17:38:04 +0100

    m

---
+++ resources/views/jeu/rules.blade.php
@@ -0,0 +1,24 @@
+@extends("base")
+
+@section('title', 'Détail du jeu')
+
+@section('content')
+
+    <div class="row justify-content-center">
+        <div class="col-6 ">
+            <h3 class="card-title">{{ $jeu->nom }}</h3>
+
+            {!! $jeu->regles !!}
+
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-6 ">
+            <a href="{{ URL::route('jeu_show', $jeu->id) }}" class="btn btn-primary">Retour àu jeu</a>
+        </div>
+        <div class="col-6 ">
+            <a href="{{ URL::route('jeu_index') }}" class="btn btn-warning">Retour à la liste des jeux</a>
+        </div>
+    </div>
+@endsection
--- resources/views/jeu/show.blade.php
+++ resources/views/jeu/show.blade.php
@@ -1,7 +1,48 @@
-<div>
-    <p><strong>Photo :</strong>{{$marathon -> nom}} </p>
-    <p><strong>Nom du jeu :</strong>{{$marathon -> nom}} </p>
-    <p><strong>Editeur :</strong>{{$marathon -> editeur_id}} </p>
-    <p><strong>Thème :</strong>{{$marathon -> theme_id}} </p>
-    <p><strong>Liste des mécaniques :</strong></p>
-</div>
+@extends("base")
+
+@section('title', 'Détail du jeu')
+
+@section('content')
+
+    <div class="row justify-content-center">
+        <div class="col-6 ">
+            <div class="card">
+                <img src="{{ url($jeu->url_media) }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $jeu->nom }}</h5>
+                    <p class="card-text">
+                    {{ $jeu->description }}
+                    <hr>
+                    {{ $jeu->theme->nom }}
+                    <hr>
+                    @foreach ( $jeu->mecaniques as $key => $mecaniques)
+                        @if ($key !== 0)
+                            &nbsp;-&nbsp;
+                        @endif
+                        {{ $mecaniques->nom }}
+                    @endforeach
+                    <hr>
+                    {{ $jeu->categorie }}
+                    <hr>
+                    Age recommandé : {{ $jeu->age }}
+                    <hr>
+                    {{ $jeu->langue }}
+                    <hr>
+                    {{ $jeu->theme->nom }}
+                    <hr>
+                    Edité par {{ $jeu->editeur->nom }}
+                    <hr>
+                    durée : {{ $jeu->duree }}
+                    <hr>
+                    Nombre de joueur : {{ $jeu->nombre_joueurs }}
+                    </p>
+                    <hr><hr>
+                    <a href="{{ URL::route('jeu_rules', $jeu->id) }}" class="btn btn-primary">Regarder les régles du jeu</a>
+                    <hr><hr>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href="{{ URL::route('jeu_index') }}" class="btn btn-secondary">Retour à la liste des jeux</a>
+@endsection
+
