85c87c8f56cbd0b69a1e05c9c0439295a1c0494e
thomasnoeson <54883700+thomasnoeson@users.noreply.github.com>
2020-12-17 10:53:24 +0100

    changement taille image

--- resources/views/jeu/index.blade.php
+++ resources/views/jeu/index.blade.php
@@ -82,7 +82,7 @@
         @foreach ($jeux as $jeu)
             <div style="margin-left: 2%;" class="col-4" >
                 <div class="card">
-                    <img src="" class="card-img-top" alt="...">
+                    <img style="width:350px; height:350px;" src="" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">{{ $jeu->nom }}</h5>
                         <p class="card-text">
