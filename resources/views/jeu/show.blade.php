2820f2ae7369cb05d4c5db65fbbd6cb9ecfb3735
henri_delbaere <henri_delbaere@ens.univ-artois.fr>
2020-12-17 09:51:10 +0100

    ex21

--- resources/views/jeu/show.blade.php
+++ resources/views/jeu/show.blade.php
@@ -67,8 +67,90 @@
                 </div>
             </div>
         </div>
-        <br>
+    @auth
+        <div class="card-body">
+            <form name="Avis" method="post" action="{{ URL::route('jeu_store') }}">
+                <div class="form-group">
+                    <label for="description">Avis</label>
+                    <div>
+                        <textarea name="description" class="form-control" required="">{{ old('description') }}</textarea>
+                    </div>
+                </div>
+                <div>
+                </div>
+                <div class="form-group">
+                    <label for="description">Note</label>
+                    <select name="Note">
+                        <option value="{{ 1 }}" selected>{{ "1/5" }}</option>
+                        <option value="{{ 2 }}" selected>{{ "2/5" }}</option>
+                        <option value="{{ 3 }}" selected>{{ "3/5" }}</option>
+                        <option value="{{ 4 }}" selected>{{ "4/5" }}</option>
+                        <option value="{{ 5 }}" selected>{{ "5/5" }}</option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        @endauth
         <h1>Commentaires:</h1>
+        <br>
+        @foreach ( $coms as $c)
+            @if ($jeu->id == $c->jeu_id)
+                <ul>
+                    <li>Auteur: {{$users->find($c->user_id)->name}}</li>
+                    <li>Commentaire: {{$c->commentaire}}</li>
+                    <li>Note: {{$c->note}}</li>
+                </ul>
+                <br>
+                @endif
+                @endforeach
+                </div>
+                17 décembre 2020
+
+                <br>
+                <div>
+                @auth
+                    <div class="card-body">
+                        <form name="Avis" method="post" action="{{ URL::route('jeu_store') }}">
+                            <div class="form-group">
+                                <label for="description">Avis</label>
+                                <div>
+                                    <textarea name="description" class="form-control" required="">{{ old('description') }}</textarea>
+                                </div>
+                            </div>
+                            <div>
+                            </div>
+                            <div class="form-group">
+                                <label for="description">Note</label>
+                                <select name="Note">
+                                    <option value="{{ 1 }}" selected>{{ "1/5" }}</option>
+                                    <option value="{{ 2 }}" selected>{{ "2/5" }}</option>
+                                    <option value="{{ 3 }}" selected>{{ "3/5" }}</option>
+                                    <option value="{{ 4 }}" selected>{{ "4/5" }}</option>
+                                    <option value="{{ 5 }}" selected>{{ "5/5" }}</option>
+                                </select>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                    @endauth
+                    </div>
+                    <h1>Commentaires:</h1>
+                    <br>
+                    @foreach ( $coms as $c)
+                        @if ($jeu->id == $c->jeu_id)
+                            <ul>
+                                <li>Auteur: {{$users->find($c->user_id)->name}}</li>
+                                <li>Commentaire: {{$c->commentaire}}</li>
+                                <li>Note: {{$c->note}}</li>
+                            </ul>
+                            <br>
+                            @endif
+                            @endforeach
+                            </div>
+                            17 décembre 2020
+
+                            <h1>Commentaires:</h1>
         <br>
         @foreach ( $coms as $c)
             @if ($jeu->id == $c->jeu_id)
